03824fa8c849872a5fb862d3cf632eedab02f0b6
唐良成 <727288093@qq.com>
2016-03-28 20:59:02 +0800

    index and menushow

---
+++ resources/common/common.js
@@ -0,0 +1,15 @@
+function datetime_to_unix(datetime) {
+    var tmp_datetime = datetime.replace(/:/g, '-');
+    tmp_datetime = tmp_datetime.replace(/ /g, '-');
+    var arr = tmp_datetime.split("-");
+    var now = new Date(Date.UTC(arr[0], arr[1] - 1, arr[2], arr[3] - 8, arr[4], arr[5]));
+    return parseInt(now.getTime() / 1000);
+}
+
+function unix_to_datetime(str) {
+	var date = new Date(str*1000);
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    D = date.getDate() + ' ';
+    return Y+M+D;
+}
--- resources/home/js/base.js
+++ resources/home/js/base.js
@@ -16,7 +16,7 @@ $(function() {
                 var datas = eval(data);
                 if (datas.type != 2) {
                     if (datas.con == "nodata") {
-                        var html = "<div class='cont p40'>没有数据</div>";
+                        var html = "<div class='cont p40'>没有数据......</div>";
                         $('.cont').replaceWith(html);
 
                     } else {
@@ -27,33 +27,23 @@ $(function() {
                         }
                     }
                 } else {
+                    if (datas.con == "nodata") {
+                        var html = "<div>没有数据......</div>";
+                        $('.clear').html(" ");
+                        $('.clear').append(html);
 
-                    // if (datas.con == "nodata") {
-                    //     // var html = "<div class='cont p40 two news'>没有数据2222222<div class='clear'></div></div>";
-                    //     var html = "<div class='clear'>没有数据2222222</div>";
-                    //     $('.clear div').remove();
-                    //     $('.cont').append(html);
+                    } else {
+                        $('.clear').html(" ");
+                        for (var i in datas) {
+                            if (datas[i]['id'] != undefined) {
+                                var datetime = unix_to_datetime(datas[i]["exchangetime"]);
 
-                    // } else {
-                    //     for (var i in datas) {
-                    //     	console.log("2");
-                    //     	$('.clear div').remove();
-                    //         if (datas[i]['id'] != undefined) {
-                    //             // var htmltime = "<span class='fr time'>" + datas[i]['exchangetime'] + "</span>",
-                    //             //     htmltitle = "<a class='article-title' href='home.php?controller=common&method=allcon&mark={$con.url}'>" + datas[i]['title'] + "</a>",
-                    //             //     htmlcontent = "<div class='m'>" + datas[i]['content'] + "</div>";
+                                var html = '<div class="team"><div class="txt"><h3><span class="fr time">' + datetime + '</span><a class="article-title" href="home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '">' + datas[i]['title'] + '</a> </h3> <div class = "m" >' + datas[i]['content'] + '</div> <a href="home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '"class="more">了解更多</a></div><div class="img"><a href = "home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '"> <img src="' + datas[i]['src'] + '" width = "318" height = "210"></a></div></div>';
+                                $('.clear').append(html);
+                            }
+                        }
 
-                    //             // var html = "<div class='team'><div class='txt'><h3></h3></div></div>";
-
-
-                    //             var html = '<div class="team"><div class="txt"><h3><span class="fr time"></span><a class="article-title" href="home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '">' + datas[i]['title'] + '</a> </h3> <div class = "m" >' + datas[i]['content'] + '</div> <a href="home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '"class="more">了解更多</a></div>< div class="img"><a href = "home.php?controller=common&method=allcon&mark=' + datas[i]['url'] + '"> <img src="' + datas[i]['src'] + '" width = "318" height = "210"></a></div></div>';
-                    //             $('.clear').append(html);
-                    //             // console.log(html);
-                    //         }
-
-                    //     }
-
-                    // }
+                    }
                 }
 
             }, "json"
